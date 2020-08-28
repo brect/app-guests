194dccbbcb8ee1def608e677d01b150cacdc2dc0
brect <b.limas93@gmail.com>
2020-08-27 23:56:06 -0300

    Initial commit

---
+++ app/src/main/java/com/blimas/guests/GuestFormActivity.kt
@@ -0,0 +1,11 @@
+package com.blimas.guests
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GuestFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_form)
+    }
+}
