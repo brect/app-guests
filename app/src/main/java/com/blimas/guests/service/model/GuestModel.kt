0718909ac378b774c8f4764a70e44357c2fa5e98
brect <b.limas93@gmail.com>
2020-08-29 01:04:53 -0300

    Alterando implementacao banco de dados

--- app/src/main/java/com/blimas/guests/service/model/GuestModel.kt
+++ app/src/main/java/com/blimas/guests/service/model/GuestModel.kt
@@ -1,7 +1,20 @@
 package com.blimas.guests.service.model
 
-data class GuestModel(
-    val id : Int = 0,
-    var name: String,
-    var presence: Boolean
-)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Guest")
+class GuestModel{
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id : Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = true
+
+}
