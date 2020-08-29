290392db104f02775b8575aa7c8d545e090c4f76
brect <b.limas93@gmail.com>
2020-08-28 23:38:07 -0300

    criado metodo atualizar

--- app/src/main/java/com/blimas/guests/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/blimas/guests/viewmodel/GuestFormViewModel.kt
@@ -1,21 +1,34 @@
 package com.blimas.guests.viewmodel
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.blimas.guests.service.model.GuestModel
 import com.blimas.guests.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mGuestRepository : GuestRepository =
-        GuestRepository()
+    private val mContext = application.applicationContext
+    private val mGuestRepository : GuestRepository = GuestRepository.getInstance(mContext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
 
-    fun save(name: String, presence:Boolean){
-        val guest = GuestModel(name, presence)
-        mGuestRepository.save(guest)
+    private var mGuest = MutableLiveData<GuestModel>()
+    val guest: LiveData<GuestModel> = mGuest
+
+    fun save(id: Int, name: String, presence:Boolean){
+        val guest = GuestModel(id, name, presence)
+        if (guest.id == 0){
+            mSaveGuest.value = mGuestRepository.save(guest)
+        }else{
+            mSaveGuest.value = mGuestRepository.update(guest)
+        }
+    }
+
+    fun load(id: Int){
+        mGuest.value = mGuestRepository.getUser(id)
     }
 }
